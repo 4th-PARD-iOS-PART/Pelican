883e1e52af42ded5049e28ff0639981772b890cd
Blackcode2 <doky03115@gamil.com>
2024-11-15 15:21:55 +0900

    Initial Commit

---
+++ PardShortathon2024-2Tests/PardShortathon2024_2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  PardShortathon2024_2Tests.swift
+//  PardShortathon2024-2Tests
+//
+//  Created by KimDogyung on 11/15/24.
+//
+
+import Testing
+@testable import PardShortathon2024_2
+
+struct PardShortathon2024_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
